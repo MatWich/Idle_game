d3757f7a180036c3544280982fa684c6af4b0489
MatWich <wicherskimateusz@gmail.com>
2021-06-06 16:46:49 +0200

    messing with progress bars

--- classes/gui/__init__.py
+++ classes/gui/__init__.py
@@ -1,2 +1,3 @@
 from .MainWindow import MainWindow
 from .UIMenu import UIMenu
+from .UIMenu import MyThread
--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import sys
 from classes import Data
 from classes import MainWindow
 from PyQt5.QtWidgets import QApplication
+from classes import MyThread
 
 
-# Press the green button in the gutter to run the script.
+def exit_handler():
+    thread1.quit()
+    thread1.join()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.aboutToQuit.connect(exit_handler)
     window = MainWindow()
-    # window.show()
+    thread1 = MyThread(window)
+    thread1.start()
     sys.exit(app.exec_())
